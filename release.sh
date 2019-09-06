77f01e952d8878e2a8aba5fdc8b52063b4237a27
Markko Paas <markko.paas@gmail.com>
2019-09-06 13:06:20 +0200

    chore: Fix release script

--- release.sh
+++ release.sh
@@ -7,22 +7,22 @@
 #create
 (cd ../fdk && git checkout develop && git pull && npm run release)
 #push
-(cd ../fdk && git checkout develop && git push --follow-tags origin)
+(cd ../fdk && git push --follow-tags origin)
 
 #fdk-api-harvester
 #create
 (cd ../fdk-api-harvester && git checkout develop && git pull && npm run release)
 #push
-(cd ../fdk-api-harvester && git checkout develop && git push --follow-tags origin)
+(cd ../fdk-api-harvester && git push --follow-tags origin)
 
 #concept-catalogue
 #create
 (cd ../concept-catalogue && git checkout develop && git pull && npm run release)
 #push
-(cd ../concept-catalogue && git checkout develop && git push --follow-tags origin)
+(cd ../concept-catalogue && git push --follow-tags origin)
 
 #concept-catalogue-gui
 #create
 (cd ../concept-catalogue-gui && git checkout develop && git pull && npm run release)
 #push
-(cd ../concept-catalogue-gui && git checkout develop && git push --follow-tags origin)
+(cd ../concept-catalogue-gui && git push --follow-tags origin)
