51af91659f360773c92ac197588be20529ffa12d
German Poljakov <gerpol17@gmail.com>
2019-10-01 13:24:45 +0200

    fix: add npm install before npm run

--- release.sh
+++ release.sh
@@ -5,24 +5,24 @@
 
 #fdk
 #create
-(cd ../fdk && git checkout develop && git pull && npm run release)
+(cd ../fdk && git checkout develop && git pull && npm i && npm run release)
 #push
 (cd ../fdk && git push --follow-tags origin)
 
 #fdk-api-harvester
 #create
-(cd ../fdk-api-harvester && git checkout develop && git pull && npm run release)
+(cd ../fdk-api-harvester && git checkout develop && git pull && npm i && npm run release)
 #push
 (cd ../fdk-api-harvester && git push --follow-tags origin)
 
 #concept-catalogue
 #create
-(cd ../concept-catalogue && git checkout develop && git pull && npm run release)
+(cd ../concept-catalogue && git checkout develop && git pull && npm i && npm run release)
 #push
 (cd ../concept-catalogue && git push --follow-tags origin)
 
 #concept-catalogue-gui
 #create
-(cd ../concept-catalogue-gui && git checkout develop && git pull && npm run release)
+(cd ../concept-catalogue-gui && git checkout develop && git pull && npm i && npm run release)
 #push
 (cd ../concept-catalogue-gui && git push --follow-tags origin)
